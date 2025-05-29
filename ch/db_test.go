06e9d6df2cc0e33ca54c51df8c8cae4e214dce4a
Minhyuk Kim <kmin1318.dev@gmail.com>
2025-05-29 12:03:22 +0900

    test: add TestNullableDateTime64

--- ch/db_test.go
+++ ch/db_test.go
@@ -303,6 +303,71 @@ func TestDateTime64(t *testing.T) {
 	require.Equal(t, in.TimeNanoPrecWithTZ.UnixNano(), out.TimeNanoPrecWithTZ.UnixNano())
 }
 
+func TestNullableDateTime64(t *testing.T) {
+	type Model struct {
+		TimeNull       *time.Time `ch:"type:Nullable(DateTime64(0))"`
+		TimeNullWithTZ *time.Time `ch:"type:Nullable(DateTime64(0, 'UTC'))"`
+
+		TimeSecPrec       *time.Time `ch:"type:Nullable(DateTime64(0))"`
+		TimeSecPrecWithTZ *time.Time `ch:"type:Nullable(DateTime64(0, 'UTC'))"`
+
+		TimeMilliPrec       *time.Time `ch:"type:Nullable(DateTime64(3))"`
+		TimeMilliPrecWithTZ *time.Time `ch:"type:Nullable(DateTime64(3, 'UTC'))"`
+
+		TimeNanoPrec       *time.Time `ch:"type:Nullable(DateTime64(9))"`
+		TimeNanoPrecWithTZ *time.Time `ch:"type:Nullable(DateTime64(9, 'UTC'))"`
+	}
+
+	ctx := context.Background()
+
+	db := chDB()
+	defer db.Close()
+
+	err := db.ResetModel(ctx, (*Model)(nil))
+	require.NoError(t, err)
+
+	timeSecPrec := time.Unix(0, 12345678912345)
+	timeSecPrecWithTZ := time.Unix(0, 12345678912345).In(time.UTC)
+
+	timeMilliPrec := time.Unix(0, 12345678912345)
+	timeMilliPrecWithTZ := time.Unix(0, 12345678912345).In(time.UTC)
+
+	timeNanoPrec := time.Unix(0, 12345678912345)
+	timeNanoPrecWithTZ := time.Unix(0, 12345678912345).In(time.UTC)
+
+	in := &Model{
+		TimeNull:       nil,
+		TimeNullWithTZ: nil,
+
+		TimeSecPrec:       &timeSecPrec,
+		TimeSecPrecWithTZ: &timeSecPrecWithTZ,
+
+		TimeMilliPrec:       &timeMilliPrec,
+		TimeMilliPrecWithTZ: &timeMilliPrecWithTZ,
+
+		TimeNanoPrec:       &timeNanoPrec,
+		TimeNanoPrecWithTZ: &timeNanoPrecWithTZ,
+	}
+	_, err = db.NewInsert().Model(in).Exec(ctx)
+	require.NoError(t, err)
+
+	out := new(Model)
+	err = db.NewSelect().Model(out).Scan(ctx)
+	require.NoError(t, err)
+
+	require.Nil(t, out.TimeNull)
+	require.Nil(t, out.TimeNullWithTZ)
+
+	require.Equal(t, timeSecPrec.Truncate(time.Second).UnixNano(), out.TimeSecPrec.UnixNano())
+	require.Equal(t, timeSecPrecWithTZ.Truncate(time.Second).UnixNano(), out.TimeSecPrecWithTZ.UnixNano())
+
+	require.Equal(t, timeMilliPrec.Truncate(time.Millisecond).UnixNano(), out.TimeMilliPrec.UnixNano())
+	require.Equal(t, timeMilliPrecWithTZ.Truncate(time.Millisecond).UnixNano(), out.TimeMilliPrecWithTZ.UnixNano())
+
+	require.Equal(t, timeNanoPrec.UnixNano(), out.TimeNanoPrec.UnixNano())
+	require.Equal(t, timeNanoPrecWithTZ.UnixNano(), out.TimeNanoPrecWithTZ.UnixNano())
+}
+
 func TestInvalidType(t *testing.T) {
 	t.Skip()
 
